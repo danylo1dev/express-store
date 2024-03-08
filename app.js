39d5f5439f09a2986a1bc1f711ab49868c239bb3
Danylo Hlushko <danylo.hlushko.dev@gmail.com>
2024-03-08 17:24:51 +0200

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const bodyParser = require("body-parser");
+const rootDir = require("./utils/path");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(rootDir, "views", "404.html"));
+});
+app.listen(3000);
